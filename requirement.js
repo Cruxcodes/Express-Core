446e970ee9bbcaaed01f4bba3686aa4d201823b0
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-09-27 10:52:28 +0100

    new changes

--- requirement.js
+++ requirement.js
@@ -1,11 +1,11 @@
-$("#distribution-service").click(function (e) { 
+$("#distribution-service ").click(function (e) { 
     e.preventDefault();
     $("#domestic-content-container").toggle("");
-    $("#cadet").toggleClass("cadet-icon-up .cadet-icon-down");
+    
  
 });
-$("#cadet").click(function (e) { 
-    $(".domestic-content-container").toggle("");
-    $("#cadet").toggleClass("cadet-icon-up .cadet-icon-down");
+$("#Courier-services").click(function (e) { 
+    $("#Courier-services-container").toggle("");
+    e.preventDefault();
     
 });
